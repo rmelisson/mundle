a2ef33676edc10d8f9b5fdb31586785a303d314c
hqxc7949 <hqxc7949@338799e7-9261-422a-836b-f85c3af29178>
2011-04-04 14:42:08 +0000

    update manifest : eclipse & frascati all
    git-svn-id: https://yourdev.rd.francetelecom.fr/svnroot/dhsa/sandbox/rmelisson/maven2bundle@925 338799e7-9261-422a-836b-f85c3af29178

--- frascati-osgi/frascati-bundles/zip-all.sh
+++ frascati-osgi/frascati-bundles/zip-all.sh
@@ -3,4 +3,5 @@ cd fa/ && zip -r fa.jar ./META-INF/ ./lib/ && cd ..
 cd fact/ && zip -r fact.jar ./META-INF/ ./lib/ && cd ..
 cd fcftoo/ && zip -r fcftoo.jar ./META-INF/ ./lib/ && cd ..
 cd juliacr/ && zip -r juliacr.jar ./META-INF/ ./lib/ && cd ..
+cp /home/remi/.m2/repository/org/ow2/frascati/frascati-assembly-factory/1.4-SNAPSHOT/frascati-assembly-factory-1.4-SNAPSHOT.jar frascatia/lib
 cd frascatia/ && zip -r frascatia.jar ./META-INF/ ./lib/ && cd ..
